5988cc6c7f41ff9d6b4cedd70a2c63414d5f10c1
orange-dinosaur <user@randomprojects.xyz>
2024-06-27 14:16:38 +0200

    Update layout to fix lateral and top navigation

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -45,127 +45,114 @@
 	}
 </script>
 
-<!-- fixed z-50 -->
+<div class="flex">
+	<!-- Left Navigation -->
+	<div class="h-dvh fixed z-10 flex items-center justify-start">
+		<!-- Links -->
+		<div class="w-full flex flex-col items-start justify-center mx-8">
+			<Button
+				href={'/users/' + user?.$id}
+				class="${$page.url.pathname === '/users/' + user?.$id
+					? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
+					: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
+			>
+				<Home size={24} />
+			</Button>
+			<Button
+				href={'/users/' + user?.$id + '/books'}
+				class="${$page.url.pathname === '/users/' + user?.$id + '/books'
+					? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
+					: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
+			>
+				<Book size={24} />
+			</Button>
+			<Button
+				href={'/users/' + user?.$id + '/bookmarks'}
+				class="${$page.url.pathname === '/users/' + user?.$id + '/bookmarks'
+					? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
+					: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
+			>
+				<Bookmark size={24} />
+			</Button>
+			<Button
+				href={'/users/' + user?.$id + '/favorites'}
+				class="${$page.url.pathname === '/users/' + user?.$id + '/favorites'
+					? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
+					: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
+			>
+				<Star size={24} />
+			</Button>
+			<Button
+				href={'/users/' + user?.$id + '/awards'}
+				class="${$page.url.pathname === '/users/' + user?.$id + '/awards'
+					? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
+					: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
+			>
+				<Award size={24} />
+			</Button>
+		</div>
+		<div class="h-full border-r-2 border-primary mb-32"></div>
+	</div>
 
-<div class="h-dvh w-dvw">
-	<div class="h-full w-full flex">
-		<div class="h-full flex flex-col items-center justify-start px-8">
+	<!-- Main content -->
+	<div class="w-full flex flex-col items-start justify-start">
+		<!-- Top Navigation -->
+		<div class="w-full fixed z-10 bg-background flex items-center justify-between py-8 px-8">
 			<!-- Logo -->
-			<a href={'/users/' + user?.$id}><img src={logo} alt="logo" class="h-11 w-auto mt-4" /></a>
+			<a href={'/users/' + user?.$id}><img src={logo} alt="logo" class="h-11 w-auto" /></a>
 
-			<!-- Links -->
-			<div class="w-auto flex flex-col items-center justify-center mt-64">
-				<Button
-					href={'/users/' + user?.$id}
-					class="${$page.url.pathname === '/users/' + user?.$id
-						? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
-						: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
-				>
-					<Home size={24} />
-				</Button>
-				<Button
-					href={'/users/' + user?.$id + '/books'}
-					class="${$page.url.pathname === '/users/' + user?.$id + '/books'
-						? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
-						: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
-				>
-					<Book size={24} />
-				</Button>
-				<Button
-					href={'/users/' + user?.$id + '/bookmarks'}
-					class="${$page.url.pathname === '/users/' + user?.$id + '/bookmarks'
-						? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
-						: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
-				>
-					<Bookmark size={24} />
-				</Button>
-				<Button
-					href={'/users/' + user?.$id + '/favorites'}
-					class="${$page.url.pathname === '/users/' + user?.$id + '/favorites'
-						? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
-						: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
-				>
-					<Star size={24} />
-				</Button>
-				<Button
-					href={'/users/' + user?.$id + '/awards'}
-					class="${$page.url.pathname === '/users/' + user?.$id + '/awards'
-						? 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full'
-						: 'h-12 min-h-12 w-12 p-0 m-0 mb-2 rounded-full bg-background text-primary hover:text-background'}"
-				>
-					<Award size={24} />
-				</Button>
+			<div class="flex items-center justify-end">
+				<!-- Search -->
+				{#if !$page.url.pathname.startsWith('/search')}
+					<form
+						on:submit={handleSearchSubmit}
+						class="w-[450px] flex border-2 border-primary items-center justify-center rounded-full pl-2 pr-1 mr-24"
+					>
+						<Input
+							id="searchString"
+							name="searchString"
+							type="search"
+							bind:value={searchString}
+							placeholder="Search..."
+							class="mr-2 my-[2px] border-none font-semibold"
+						/>
+						<Button type="submit" class="h-9 min-h-9 w-9 min-w-9 p-0 m-0 rounded-full">
+							<Search size={16} />
+						</Button>
+					</form>
+				{/if}
+
+				<!-- Avatar dropdown menu -->
+				<DropdownMenu.Root>
+					<DropdownMenu.Trigger asChild let:builder>
+						<Button
+							variant="ghost"
+							builders={[builder]}
+							class="relative h-11 w-11 rounded-full mr-8"
+						>
+							<Avatar.Root class="h-11 w-11">
+								<Avatar.Image src={avatarUrl} alt="user profile picture" />
+								<Avatar.Fallback>SC</Avatar.Fallback>
+							</Avatar.Root>
+						</Button>
+					</DropdownMenu.Trigger>
+					<DropdownMenu.Content class="" align="end">
+						<DropdownMenu.Label class="font-normal mr-2 mt-3">
+							<div class="flex flex-col space-y-1">
+								<p class="text-sm font-medium leading-none">{user?.name}</p>
+								<p class="text-xs leading-none text-muted-foreground">{user?.email}</p>
+							</div>
+						</DropdownMenu.Label>
+						<DropdownMenu.Separator />
+						<DropdownMenu.Group>
+							<DropdownMenu.Item>Settings</DropdownMenu.Item>
+						</DropdownMenu.Group>
+						<DropdownMenu.Separator />
+						<DropdownMenu.Item on:click={logout}>Log out</DropdownMenu.Item>
+					</DropdownMenu.Content>
+				</DropdownMenu.Root>
 			</div>
 		</div>
-
-		<div class=" w-0 border-r-2 border-primary my-4"></div>
-
-		<div class="w-full flex flex-col items-start justify-start">
-			<div class="w-full flex flex-col">
-				<div class="flex items-center justify-between py-4 bg-background">
-					<!-- App name -->
-					<div>
-						<h1 class="font-bold text-primary text-2xl ml-8">tyna</h1>
-					</div>
-
-					<div class="w-full flex items-center justify-end">
-						<!-- Search -->
-						{#if !$page.url.pathname.startsWith('/search')}
-							<form
-								on:submit={handleSearchSubmit}
-								class="w-[450px] flex border-2 border-primary items-center justify-center rounded-full pl-2 pr-1 mr-24"
-							>
-								<Input
-									id="searchString"
-									name="searchString"
-									type="search"
-									bind:value={searchString}
-									placeholder="Search..."
-									class="mr-2 my-[2px] border-none font-semibold"
-								/>
-								<Button type="submit" class="h-9 min-h-9 w-9 min-w-9 p-0 m-0 rounded-full">
-									<Search size={16} />
-								</Button>
-							</form>
-						{/if}
-
-						<!-- Avatar dropdown menu -->
-						<DropdownMenu.Root>
-							<DropdownMenu.Trigger asChild let:builder>
-								<Button
-									variant="ghost"
-									builders={[builder]}
-									class="relative h-11 w-11 rounded-full mr-8"
-								>
-									<Avatar.Root class="h-11 w-11">
-										<Avatar.Image src={avatarUrl} alt="user profile picture" />
-										<Avatar.Fallback>SC</Avatar.Fallback>
-									</Avatar.Root>
-								</Button>
-							</DropdownMenu.Trigger>
-							<DropdownMenu.Content class="w-56" align="end">
-								<DropdownMenu.Label class="font-normal">
-									<div class="flex flex-col space-y-1">
-										<p class="text-sm font-medium leading-none">{user?.name}</p>
-										<p class="text-xs leading-none text-muted-foreground">{user?.email}</p>
-									</div>
-								</DropdownMenu.Label>
-								<DropdownMenu.Separator />
-								<DropdownMenu.Group>
-									<DropdownMenu.Item>Profile</DropdownMenu.Item>
-									<DropdownMenu.Item>Settings</DropdownMenu.Item>
-								</DropdownMenu.Group>
-								<DropdownMenu.Separator />
-								<DropdownMenu.Item on:click={logout}>Log out</DropdownMenu.Item>
-							</DropdownMenu.Content>
-						</DropdownMenu.Root>
-					</div>
-				</div>
-			</div>
-
-			<div class="h-full w-full pl-8 pt-8 z-0">
-				<slot />
-			</div>
-		</div>
+		<div class="h-full ml-32 mt-32"><slot /></div>
 	</div>
 </div>
