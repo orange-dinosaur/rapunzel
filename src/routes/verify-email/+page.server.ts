99ae6e4b9a3b6c0a136bcd8579a1af190a21d318
orange-dinosaur <user@randomprojects.xyz>
2024-07-04 00:25:07 +0200

    refactor: Redirect to user's home page if email is verified
    If the user is logged in and their email is verified, the `load` function in `verify-email/+page.server.ts` now redirects them to their home page. This ensures that verified users are directed to the appropriate page upon verification.

--- src/routes/verify-email/+page.server.ts
+++ src/routes/verify-email/+page.server.ts
@@ -1,7 +1,13 @@
 import { createClient } from '$lib/server/appwrite';
 import type { AppwriteException } from 'node-appwrite';
+import { redirect } from '@sveltejs/kit';
 
-export async function load({ url }) {
+export async function load({ locals, url }) {
+	if (locals.user && locals.user.emailVerification) {
+		// If the user is logged in and is email is verified, redirect to its home page.
+		redirect(301, `/users/${locals.user.$id}`);
+	}
+
 	const userId = url.searchParams.get('userId')?.toString() ?? '';
 	const secret = url.searchParams.get('secret')?.toString() ?? '';
 
