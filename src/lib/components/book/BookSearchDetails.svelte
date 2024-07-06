df27d58f2d1f2a875038e7bfbcb96e74dcaa8b68
orange-dinosaur <user@randomprojects.xyz>
2024-07-06 02:30:46 +0200

    Add scroll area to book details if there is overflow

--- src/lib/components/book/BookSearchDetails.svelte
+++ src/lib/components/book/BookSearchDetails.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
 	import { ScrollArea } from '$lib/components/ui/scroll-area';
-	import { Badge } from '$lib/components/ui/badge';
 	import * as Select from '$lib/components/ui/select';
 	import * as Sheet from '$lib/components/ui/sheet';
 	import { BookSaved } from '$lib/types/book/book';
@@ -22,9 +21,6 @@
 	function handleRatingChange(newRating: number) {
 		rating = newRating;
 	}
-
-	let tag = $state('');
-	let isAddingTag = $state(false);
 
 	async function saveBookToLibrary() {
 		const bookToSave = new BookSaved({
@@ -59,111 +55,113 @@
 	<Sheet.Root>
 		<Sheet.Trigger class="text-primary text-sm font-semibold">+ Details</Sheet.Trigger>
 		<Sheet.Content class="min-w-[800px] p-16">
-			<div class="flex w-full items-start justify-start">
-				<!-- Book Cover -->
-				<img src={book.cover} alt={book.title} class="h-60 w-auto mb-8" />
+			<ScrollArea class="h-full w-full">
+				<div class="flex w-full items-start justify-start">
+					<!-- Book Cover -->
+					<img src={book.cover} alt={book.title} class="h-60 w-auto mb-8" />
 
-				<!-- Actions (Add to library or Save to whishlist) -->
-				<div class="h-60 flex flex-col justify-end ml-4">
-					<Button class="mb-2" on:click={saveBookToLibrary}>Add to library</Button>
-					<Button variant="outline">Save to whishlist</Button>
+					<!-- Actions (Add to library or Save to whishlist) -->
+					<div class="h-60 flex flex-col justify-end ml-4">
+						<Button class="mb-2" on:click={saveBookToLibrary}>Add to library</Button>
+						<Button variant="outline">Save to whishlist</Button>
+					</div>
 				</div>
-			</div>
 
-			<Sheet.Header>
-				<!-- Book Title -->
-				<Sheet.Title>{book.title}</Sheet.Title>
+				<Sheet.Header>
+					<!-- Book Title -->
+					<Sheet.Title>{book.title}</Sheet.Title>
 
-				<Sheet.Description>
-					<!-- Book Authors and Publishing info -->
-					<p class="text-base text-muted-foreground">{book.authors}</p>
-					<div class="flex mt-2 mb-4">
-						{#if book.publisher}
-							<p class="text-sm text-muted-foreground mr-2">{book.publisher}</p>
-						{/if}
-						<p class="text-sm text-muted-foreground">({book.publishedDate})</p>
+					<Sheet.Description>
+						<!-- Book Authors and Publishing info -->
+						<p class="text-base text-muted-foreground">{book.authors}</p>
+						<div class="flex mt-2 mb-4">
+							{#if book.publisher}
+								<p class="text-sm text-muted-foreground mr-2">{book.publisher}</p>
+							{/if}
+							<p class="text-sm text-muted-foreground">({book.publishedDate})</p>
+						</div>
+
+						<!-- Book Details: ISBN, Pages and Language -->
+						<div class="flex flex-col items-start">
+							<div class="flex mb-1">
+								<p class="text-sm text-muted-foreground font-semibold mr-2">ISBN:</p>
+								<p class="text-sm text-muted-foreground">
+									{book.isbn13 != '' ? book.isbn13 : book.isbn10}
+								</p>
+							</div>
+							<div class="flex mb-1">
+								<p class="text-sm text-muted-foreground font-semibold mr-2">Pages:</p>
+								<p class="text-sm text-muted-foreground">
+									{book.pageCount}
+								</p>
+							</div>
+							<div class="flex">
+								<p class="text-sm text-muted-foreground font-semibold mr-2">Language:</p>
+								<p class="text-sm text-muted-foreground">
+									{book.language}
+								</p>
+							</div>
+						</div>
+
+						<!-- Book Description -->
+						<ScrollArea class="min-h-40 h-32 w-full text-sm text-muted-foreground mt-6"
+							>{book.description}</ScrollArea
+						>
+					</Sheet.Description>
+				</Sheet.Header>
+
+				<div class="flex flex-col mt-6 border-t-2 border-primary">
+					<!-- Reading Status -->
+					<div class="flex items-center mt-6">
+						<p class="text-sm text-muted-foreground font-semibold mr-2">Reading status:</p>
+						<Select.Root
+							selected={readingStatus ? { label: readingStatus, value: readingStatus } : undefined}
+							onSelectedChange={(v) => {
+                                v && (readingStatus = v.value as string);
+                            }}
+						>
+							<Select.Trigger class="w-[180px] text-sm">
+								<Select.Value placeholder={readingStatus as string} />
+							</Select.Trigger>
+							<Select.Content>
+								<Select.Item value="to-read">to-read</Select.Item>
+								<Select.Item value="reading">reading</Select.Item>
+								<Select.Item value="completed">completed</Select.Item>
+								<Select.Item value="dnf">dnf</Select.Item>
+							</Select.Content>
+						</Select.Root>
 					</div>
 
-					<!-- Book Details: ISBN, Pages and Language -->
-					<div class="flex flex-col items-start">
-						<div class="flex mb-1">
-							<p class="text-sm text-muted-foreground font-semibold mr-2">ISBN:</p>
-							<p class="text-sm text-muted-foreground">
-								{book.isbn13 != '' ? book.isbn13 : book.isbn10}
-							</p>
-						</div>
-						<div class="flex mb-1">
-							<p class="text-sm text-muted-foreground font-semibold mr-2">Pages:</p>
-							<p class="text-sm text-muted-foreground">
-								{book.pageCount}
-							</p>
-						</div>
-						<div class="flex">
-							<p class="text-sm text-muted-foreground font-semibold mr-2">Language:</p>
-							<p class="text-sm text-muted-foreground">
-								{book.language}
-							</p>
-						</div>
+					<!-- Book Type -->
+					<div class="flex items-center mt-1 mb-6">
+						<p class="text-sm text-muted-foreground font-semibold mr-2">Book type:</p>
+						<Select.Root
+							selected={bookType ? { label: bookType, value: bookType } : undefined}
+							onSelectedChange={(v) => {
+                            v && (bookType = v.value as string);
+                        }}
+						>
+							<Select.Trigger class="w-[180px] text-sm">
+								<Select.Value placeholder={bookType} />
+							</Select.Trigger>
+							<Select.Content>
+								<Select.Item value="book">book</Select.Item>
+								<Select.Item value="e-book">e-book</Select.Item>
+								<Select.Item value="audiobook">audiobook</Select.Item>
+							</Select.Content>
+						</Select.Root>
 					</div>
 
-					<!-- Book Description -->
-					<ScrollArea class="min-h-40 h-32 w-full text-sm text-muted-foreground mt-6"
-						>{book.description}</ScrollArea
-					>
-				</Sheet.Description>
-			</Sheet.Header>
+					<!-- Tags -->
+					<Tags {tags} onTagsChange={handleTagsChange} />
 
-			<div class="flex flex-col mt-6 border-t-2 border-primary">
-				<!-- Reading Status -->
-				<div class="flex items-center mt-6">
-					<p class="text-sm text-muted-foreground font-semibold mr-2">Reading status:</p>
-					<Select.Root
-						selected={readingStatus ? { label: readingStatus, value: readingStatus } : undefined}
-						onSelectedChange={(v) => {
-							v && (readingStatus = v.value as string);
-						}}
-					>
-						<Select.Trigger class="w-[180px] text-sm">
-							<Select.Value placeholder={readingStatus as string} />
-						</Select.Trigger>
-						<Select.Content>
-							<Select.Item value="to-read">to-read</Select.Item>
-							<Select.Item value="reading">reading</Select.Item>
-							<Select.Item value="completed">completed</Select.Item>
-							<Select.Item value="dnf">dnf</Select.Item>
-						</Select.Content>
-					</Select.Root>
+					<!-- Rating -->
+					<div class="flex mt-8">
+						<!-- <StarRating /> -->
+						<StarRating {rating} onRatingChange={handleRatingChange} stars="5" />
+					</div>
 				</div>
-
-				<!-- Book Type -->
-				<div class="flex items-center mt-1 mb-4">
-					<p class="text-sm text-muted-foreground font-semibold mr-2">Book type:</p>
-					<Select.Root
-						selected={bookType ? { label: bookType, value: bookType } : undefined}
-						onSelectedChange={(v) => {
-                        v && (bookType = v.value as string);
-                    }}
-					>
-						<Select.Trigger class="w-[180px] text-sm">
-							<Select.Value placeholder={bookType} />
-						</Select.Trigger>
-						<Select.Content>
-							<Select.Item value="book">book</Select.Item>
-							<Select.Item value="e-book">e-book</Select.Item>
-							<Select.Item value="audiobook">audiobook</Select.Item>
-						</Select.Content>
-					</Select.Root>
-				</div>
-
-				<!-- Tags -->
-				<Tags {tags} onTagsChange={handleTagsChange} />
-
-				<!-- Rating -->
-				<div class="flex mt-4">
-					<!-- <StarRating /> -->
-					<StarRating {rating} onRatingChange={handleRatingChange} stars="5" />
-				</div>
-			</div>
+			</ScrollArea>
 		</Sheet.Content>
 	</Sheet.Root>
 </div>
